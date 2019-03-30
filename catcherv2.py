7cebd62386ff0b7b9b8335c9384ca3c8dbcabcd6
heeshung <heeshung@gmail.com>
2019-03-30 00:30:58 -0400

    made filtered hexcodes highest priority, now supersedes desiredtraits.  added new filtered airlines and hexcodes.

--- catcherv2.py
+++ catcherv2.py
@@ -238,11 +238,11 @@ class catcher(Daemon):
 		"JME","WSN","FSY","GRN","CST","TAY","XSR","TFF","GEC","RLI","XLS","BVR","LXJ","MHV","CFG","VLZ","SIO",
 		"LDX","LXG","DSO","ICV","AOJ","ADN","VCG","JAF","LEA","JEF","GMA","IJM","SVW","EXU","GES","VCN","DCS",
 		"MLM","ABW","ANZ","EAU","RCH","PAT","EXS","KNT","CAZ","KCE","EDW","NDR","KFE","KTK","SHE","GLJ","JET",
-		"EAV","PNC","NDL","JCL","AYY","FYL","ULC","FEX","ABP","JDI","TEU","FPG","MJF","EUW","BMW","RZO"]
+		"EAV","PNC","NDL","JCL","AYY","FYL","ULC","FEX","ABP","JDI","TEU","FPG","MJF","EUW","BMW","RZO","PBR"]
 		filteredcountries=["USCiv","USMil"]
 		filteredhexcodes=["780926","7101e0","0c4146","0aa004","0aa001","0d8137","780979","564795","500141","60698b",
 		"780705","293d7c","0c414f","0c4152","0c414c","0c414b","0c4153","0c4154","0d0aea","5000ce","5000e6","500141",
-		"50012a","50012a","780043","43ec0e","7103e8"]
+		"50012a","50012a","780043","43ec0e","7103e8","0ac379","0d823b","5000c0","500155"]
 		
 		desiredregs=["N367HP","N80991","N851TB","N8050J","N6189Q","N4468N","C-GXII","C-FWTF"]
 		desiredairlines=["WWW","MPE","BOE","RRR"]
@@ -288,9 +288,13 @@ class catcher(Daemon):
 					if (hasregistration==True):
 						if ((nreg in desiredregs)==True):
 							desiredtraits=True
+					#filter out specific hex codes first
+					if ((line[12:18] in filteredhexcodes)==True):
+						process=False
+
 					#filter out countries and common airlines and for flights without numbers
-					if (desiredtraits==False):
-						if ((hexcountry in filteredcountries)==True or (line[30:33] in filteredairlines)==True or (line[12:18] in filteredhexcodes)==True or any(char.isdigit() for char in line[30:38])==False):
+					elif (desiredtraits==False):
+						if ((hexcountry in filteredcountries)==True or (line[30:33] in filteredairlines)==True or any(char.isdigit() for char in line[30:38])==False):
 							process=False
 		
 					
