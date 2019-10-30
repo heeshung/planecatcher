b9c6e56515e27feb12117a21908bc83b1f09add5
heeshung <heeshung@gmail.com>
2019-10-29 23:12:12 -0400

    added stderr logging to daemon

--- daemon.py
+++ daemon.py
@@ -9,7 +9,7 @@ class Daemon:
        
         Usage: subclass the Daemon class and override the run() method
         """
-        def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/root/planecatcher/log/'+time.strftime("%Y%m%d-%H:%M:%S")+'-error.log'):
                 self.stdin = stdin
                 self.stdout = stdout
                 self.stderr = stderr
